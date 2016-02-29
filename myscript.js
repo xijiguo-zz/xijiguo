994222a68ebcdf4109cac93f09f8721683405a7b
yguoo <xguo2008@gmail.com>
2016-02-29 09:28:50 -0500

    finished main page

---
+++ myscript.js
@@ -0,0 +1,126 @@
+(function () {
+
+	var HEIGHT = window.innerHeight;
+	var WIDTH = window.innerWidth;
+	var HEADEROFFSET = 50;
+
+	// Initialization DOM is ready
+	document.addEventListener("DOMContentLoaded", function() {
+		var body = document.querySelector("body");
+		body.setAttribute("class", "init");
+		mainSetup();
+		menuSetup();
+		body.className = "";
+	})
+
+	// Document scrolling freatures
+	document.addEventListener("scroll", function(event) {
+		var notAtTop = window.pageYOffset;
+		var header = document.querySelector("#header");
+
+		// Header box-shadow
+		if (notAtTop) {
+			header.style.boxShadow = "0 0 4px 4px rgba(0, 0, 0, 0.4)";
+		} else {
+			header.style.boxShadow = "none";
+		}
+	})
+
+	// Window resizing
+	window.addEventListener("resize", function() {
+		mainSetup();
+	})
+
+	// Helper functions
+	// Main page setup
+	function mainSetup() {
+		HEIGHT = window.innerHeight;
+
+		var main = document.getElementById("main");
+		main.style.height = HEIGHT - HEADEROFFSET + "px";
+
+		var about = document.querySelector("#about");
+		var aboutPos = about.offsetTop - HEADEROFFSET;
+
+		main.addEventListener("click", function(event) {
+			smoothScroll(window.scrollY, aboutPos);
+		})
+	}
+
+	// Menu setup
+	function menuSetup() {
+		var menu = document.querySelectorAll("#header-list li");
+		var item, goToItem, goToItemPos;
+		for (var i = 0; i < menu.length; i++) {
+			item = menu[i]
+			switch(item.textContent) {
+				case "ABOUT":
+					item.addEventListener("click", function(){
+						goToItem = document.querySelector("#about");
+						goToItemPos = goToItem.offsetTop - HEADEROFFSET;
+						smoothScroll(window.scrollY, goToItemPos);
+					});
+					break;
+				case "PORTFOLIO":
+					item.addEventListener("click", function(){
+						goToItem = document.querySelector("#portfolio");
+						goToItemPos = goToItem.offsetTop - HEADEROFFSET;
+						smoothScroll(window.scrollY, goToItemPos);
+					});
+					break;
+				case "CODE":
+					item.addEventListener("click", function(){
+						goToItem = document.querySelector("#code");
+						goToItemPos = goToItem.offsetTop - HEADEROFFSET;
+						smoothScroll(window.scrollY, goToItemPos);
+					});
+					break;
+				case "CONTRACT":
+					item.addEventListener("click", function(){
+						goToItem = document.querySelector("#contact");
+						goToItemPos = goToItem.offsetTop - HEADEROFFSET;
+						smoothScroll(window.scrollY, goToItemPos);
+					});
+					break;
+				default:
+			}
+		}
+	}
+
+	// Smooth scroll
+	function smoothScroll(begin, end) {
+		if (begin < end) scrollDown(begin, end);
+		else scrollUp(begin, end);
+	}
+	function scrollDown(begin, end) {
+		if (begin < end) {
+			if (begin + 15 >= end) {
+				setTimeout(function(){
+					window.scrollTo(0, begin + 1);
+					smoothScroll(begin+1, end);
+				}, 1)
+			} else {
+				setTimeout(function(){
+					window.scrollTo(0, begin + 15);
+					smoothScroll(begin+15, end);
+				}, 1)
+			}
+		}
+	}
+	function scrollUp(begin, end) {
+		if (begin > end) {
+			if (begin - 15 <= end) {
+				setTimeout(function(){
+					window.scrollTo(0, begin - 1);
+					smoothScroll(begin-1, end);
+				}, 1)
+			} else {
+				setTimeout(function(){
+					window.scrollTo(0, begin - 15);
+					smoothScroll(begin-15, end);
+				}, 1)
+			}
+		}
+	}
+
+})();
